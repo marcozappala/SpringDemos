0a2111d5f25d92982dd8190eca3b686ae87691a0
mzappala <marco.m.zappala@gmail.com>
2018-10-07 22:34:45 +0200

    binding of mapOfMap

---
+++ src/main/java/com/example/demo/ZapMapOfMap.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties
+@ConfigurationProperties(prefix = "zap-map-of-map")
+public class ZapMapOfMap {
+    private Map<String, Map<String, String>> map = new HashMap<>();
+
+    public Map<String, Map<String, String>> getMap() {
+        return map;
+    }
+
+    public void setMap(Map<String, Map<String, String>> map) {
+        this.map = map;
+    }
+}
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -18,9 +18,23 @@ public class DemoApplicationTests {
     @Autowired
     private ZapMap zapMap;
 
+    @Autowired
+    private ZapMapOfList zapMapOfList;
+
+    @Autowired
+    private ZapMapOfMap zapMapOfMap;
+
     @Test
     public void contextLoads() {
         for (Map.Entry<String, String> entry: zapMap.getMappa().entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue().toString());
+        }
+
+        for (Map.Entry<String, List<String>> entry: zapMapOfList.getWhatever().entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue().toString());
+        }
+
+        for (Map.Entry<String, Map<String,String>> entry: zapMapOfMap.getMap().entrySet()) {
             System.out.println(entry.getKey() + " " + entry.getValue().toString());
         }
     }
